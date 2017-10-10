0076cc8492b760eaf4bb0ab86e1cdd775bc2fea7
cadderley <cadderley@urthecast.com>
2017-10-10 11:42:52 -0700

    Revise cost numbers for 1.25m parts

--- Source/HabUtils/ModuleAdjustableLeg.cs
+++ Source/HabUtils/ModuleAdjustableLeg.cs
@@ -11,17 +11,20 @@ namespace HabUtils
   public class ModuleAdjustableLeg:PartModule
   {
 
+    // Lcoalized leg display name
+    public string LegDisplayName = "Leg01";
     // Name to refer to the leg
     public string LegID = "Leg01";
 
     // Name of the object that will be extending
     public string ExtenderTransformName;
 
-    // Fraction of maximum extension
+    // Current fraction of maximum extension
     public float ExtensionFraction = 0.0f;
 
     // Minimum extension, from model local position, in units
     public float ExtenderMin = 0.0f;
+
     // Maximum extension, from model local position, in units
     public float ExtenderMax = 1.0f;
 
